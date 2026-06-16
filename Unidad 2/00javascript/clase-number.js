d590e10489eb258082705db7f05e1867a9e9ef3e
StevenPerez2304 <steven.r.perez@unl.edu.ec>
2026-06-16 11:34:54 -0500

    feat: Clase del 16/06/2026 Raul Perez

---
+++ Unidad 2/00javascript/clase-number.js
@@ -0,0 +1,28 @@
+
+const entero = 42
+const decimal = 3.14
+console.log(typeof entero, typeof decimal)
+
+//notacion cientifica
+const cientifica = 5e3
+
+//Representacion de otros textos 
+
+const infinito = Infinity
+const noEsUnNumero = NaN
+
+const multiplicacion = 4 * 7
+
+//Modulps de exponenciacion
+const modulo = 15 % 8
+const exponenciacion = 0.1 + 0.2
+
+console.log(exponenciacion)
+console.log(exponenciacion.toFixed(1)) 
+
+const raizcuadrada = Math.sqrt(16)
+const valorAbsoluto = Math.abs(-5) 
+const aleatorio = Math.random()
+console.log(raizcuadrada)
+console.log(valorAbsoluto)
+console.log(aleatorio)
--- Unidad 2/00javascript/tipos-datos.js
+++ Unidad 2/00javascript/tipos-datos.js
@@ -1,5 +1,41 @@
-//symbol valor unico 
+// Primitivos
+let nombre = "Luis"
 
-let simboloUnico = Symbol("unico");
+//Numerico
+let edad = 20
 
-let numeroGrande = 2n   
+//Booleanos
+let esMayor = true
+
+// null
+let noHayValor = null
+
+//undefined
+let noDefinido = undefined
+
+//Simbolos de valor unico
+let simbolo = Sumbol('unico')
+
+//datos gigantes bigint
+let numeroGrande = 2n
+
+
+// Tipos de datos COMPLETOS
+
+//object
+let carro = {
+    marca: 'Lamborghini',
+    modelo: 'Aventador'
+}
+
+// arrays
+let frutas = [
+    'Manzana',
+    'Pera',
+    'Sandia'
+]
+
+// Funciones
+function saludar() {
+    
+}
